c5e5c4a87f7beacadd3687a6dc3d28cfac7b7ef8
Mathias Maes <mathiasmaesdev@gmail.com>
2020-03-23 10:59:02 +0100

    CHA: text size

--- Scenes/GameOverScene.cpp
+++ Scenes/GameOverScene.cpp
@@ -8,17 +8,17 @@ void SpaceInvaders::Scenes::GameOverScene::load()
 {
     auto* fontLoader = GameController::getInstance().getFontLoader();
 
+    // Create game over title
     auto* gameOverTxt = GameController::getInstance().getFactory()
-            ->createText("Game Over",fontLoader->getFont("bold"), Color(255,255,255), 0, 0);
-
-    //TODO: WHAT DO THE NUMBERS MEAN
+            ->createText("Game Over",fontLoader->getFont("bold"), Color(255,255,255), 0, 0, 2, 2);
 
     gameOverTxt->setPosition(std::lround( (WINDOW_SIZE_X - gameOverTxt->getSize()[0]) / 2.0),
             gameOverTxt->getPosition()[1]);
 
+    // Create points scored text
     auto* pointsTxt = GameController::getInstance().getFactory()
             ->createText("You scored: 0 pts",fontLoader->getFont("regular"), Color(255,255,255), 0,
-                    std::lround(gameOverTxt->getSize()[1] + 30 * SCALE_Y));
+                    std::lround(gameOverTxt->getSize()[1]));
 
     pointsTxt->setPosition(std::lround((WINDOW_SIZE_X - pointsTxt->getSize()[0])/2.0),
                            pointsTxt->getPosition()[1]);
